a3cfdc76dbd53ae9784f1f51b1f323d9df6bd70c
Alex <alejandrourdaneta1213@hotmail.com>
2023-06-18 09:46:29 -0400

    funcionalidad de tarjetas añadidas

--- card.js
+++ card.js
@@ -1,5 +1,15 @@
-const cardsShow = document.querySelectorAll(".card__show");
+const group1 = document.getElementById("group1");
+const group2 = document.getElementById("group2");
+const group3 = document.getElementById("group3");
 
-cardsShow.forEach(card, ()=>{
-    card.addEventListener
-})
+group1.addEventListener("click", (e)=>{
+    document.querySelector(".card__hidden1").classList.toggle("display");
+});
+
+group2.addEventListener("click", (e)=>{
+    document.querySelector(".card__hidden2").classList.toggle("display");
+});
+
+group3.addEventListener("click", (e)=>{
+    document.querySelector(".card__hidden3").classList.toggle("display");
+});
